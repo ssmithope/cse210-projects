8b5b7458b8522412277bcbf9efdd9f8332a52a70
Smith-Eutrope-Zephirin <ssmithope@gmail.com>
2024-04-27 20:59:14 -0500

    Prep3 done with the dowhile loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+        // I've tried the dowhile loop.
+        // Get a majic number from the user.
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 20);
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is your magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (magic_number != guess);
+        Console.WriteLine("You guessed it!");
+        
     }
 }
