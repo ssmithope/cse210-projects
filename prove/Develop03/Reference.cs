fe0d82dec6c47cb24c7cc524818f327ad5f7fab8
Smith-Eutrope-Zephirin <ssmithope@gmail.com>
2024-05-16 04:48:05 -0500

    Prove w03 resubmission

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,15 @@
+// A reference class created.
+class Reference
+{
+    private string _text;
+
+    public Reference(string text)
+    {
+        _text = text;
+    }
+
+    public string Text
+    {
+        get { return _text; }
+    }
+}
